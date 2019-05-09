dcaed80a76e40926382a573b0483200c9b045c66
Yauhen Kviatkouski <kvetkovski@gmail.com>
2019-05-09 14:02:17 +0300

    feat: add opportunity to get info about clips according users request

--- youtube-client/src/controllers/Controller.js
+++ youtube-client/src/controllers/Controller.js
@@ -1,7 +1,15 @@
 import ViewStart from '../views/AppViews/ViewStart';
+import ModelRequest from '../models/Model';
 
 export default class Controller {
   static start() {
     ViewStart();
+    document.querySelector('input').addEventListener('keydown', (e) => {
+      if (e.code === 'Enter') {
+        const userRequest = document.querySelector('input').value;
+        const model = new ModelRequest(userRequest);
+        model.getClips();
+      }
+    });
   }
 }
